bbc2f4409958ad23f74e0b3744a33e66f7fa7ada
çağatay kılınç <107580070+klncgty@users.noreply.github.com>
2024-03-10 18:34:43 +0300

    Update config.py

--- Cache_Bot/config.py
+++ Cache_Bot/config.py
@@ -2,7 +2,7 @@ import os
 
 def Config():
     config={
-            "token":'MTIwNjkwNjQ4MTM0MjAyNTc2OQ.GGy_-F.Kd8qxdG46m5Vksglx9vUORzuKsbbc3Y83KCpF4',
+            "token":'buraya_discord_token_gelecek',
             "command_pref":"!!",
             "permissions":66560,
             "output_url" : f'{os.path.dirname(os.path.realpath(__name__))}/Output_CSVs'
